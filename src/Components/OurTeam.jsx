914239a61534abc06600bc4ead918c51f8a8aa01
Md Tanzil <mtanzil1999@gmail.com>
2023-10-08 07:00:08 +0600

    team section add

---
+++ src/Components/OurTeam.jsx
@@ -0,0 +1,121 @@
+
+
+const OurTeam = () => {
+    return (
+      <div>
+        <h1 className="text-4xl mb-10 text-black text-center font-bold roboto capitalize">
+          Our team
+        </h1>
+        <h3
+          className="text-
+        xl mb-4 text-[#040a17] text-center font-medium roboto "
+        >
+          Loved by businesses and individuals across the globe.
+        </h3>
+        <div className="grid lg:grid-cols-4  grid-cols-1 gap-4">
+          <div className="p-4 shadow-xl m-5">
+            <img
+              src="https://joinup.qodeinteractive.com/wp-content/uploads/2020/11/p1-team-1.jpg"
+              alt=""
+              className="h-64 w-64 rounded-2xl"
+            />
+            <h3 className="barlow text-xl font-semibold text-[#040a17] my-3">
+              Keisha Chapman
+            </h3>
+            <p className="barlow text-sm font-medium text-[#a9a9a9]">
+              Manager
+            </p>
+          </div>
+          <div className="p-4 shadow-xl m-5">
+            <img
+              src="https://joinup.qodeinteractive.com/wp-content/uploads/2020/11/p1-team-6.jpg"
+              alt=""
+              className="h-64 w-64 rounded-2xl"
+            />
+            <h3 className="barlow text-xl font-semibold text-[#040a17] my-3">
+              Ritik Hughes
+            </h3>
+            <p className="barlow text-sm font-medium text-[#a9a9a9]">
+              Social Media Manager
+            </p>
+          </div>
+          <div className="p-4 shadow-xl m-5">
+            <img
+              src="https://joinup.qodeinteractive.com/wp-content/uploads/2020/11/p1-team-3.jpg"
+              alt=""
+              className="h-64 w-64 rounded-2xl"
+            />
+            <h3 className="barlow text-xl font-semibold text-[#040a17] my-3">
+              Larry Barnett
+            </h3>
+            <p className="barlow text-sm font-medium text-[#a9a9a9]">
+              Developer
+            </p>
+          </div>
+          <div className="p-4 shadow-xl m-5">
+            <img
+              src="https://joinup.qodeinteractive.com/wp-content/uploads/2020/11/p1-team-4.jpg"
+              alt=""
+              className="h-64 w-64 rounded-2xl"
+            />
+            <h3 className="barlow text-xl font-semibold text-[#040a17] my-3">
+              Derren Morley
+            </h3>
+            <p className="barlow text-sm font-medium text-[#a9a9a9]">CEO</p>
+          </div>
+          <div className="p-4 shadow-xl m-5">
+            <img
+              src="https://joinup.qodeinteractive.com/wp-content/uploads/2020/11/p1-team-5.jpg"
+              alt=""
+              className="h-64 w-64 rounded-2xl"
+            />
+            <h3 className="barlow text-xl font-semibold text-[#040a17] my-3">
+              Jordan-Lee Meyers
+            </h3>
+            <p className="barlow text-sm font-medium text-[#a9a9a9]">
+             Interior Designer
+            </p>
+          </div>
+          <div className="p-4 shadow-xl m-5">
+            <img
+              src="https://joinup.qodeinteractive.com/wp-content/uploads/2020/11/p1-team-6.jpg"
+              alt=""
+              className="h-64 w-64 rounded-2xl"
+            />
+            <h3 className="barlow text-xl font-semibold text-[#040a17] my-3">
+              Carla Schmitt
+            </h3>
+            <p className="barlow text-sm font-medium text-[#a9a9a9]">
+              Executive Manager
+            </p>
+          </div>
+          <div className="p-4 shadow-xl m-5">
+            <img
+              src="https://joinup.qodeinteractive.com/wp-content/uploads/2020/11/p1-team-7.jpg"
+              alt=""
+              className="h-64 w-64 rounded-2xl"
+            />
+            <h3 className="barlow text-xl font-semibold text-[#040a17] my-3">
+              Connie Mercado
+            </h3>
+            <p className="barlow text-sm font-medium text-[#a9a9a9]">
+              Designer
+            </p>
+          </div>
+          <div className="p-4 shadow-xl m-5">
+            <img
+              src="https://joinup.qodeinteractive.com/wp-content/uploads/2020/11/p1-team-8.jpg"
+              alt=""
+              className="h-64 w-64 rounded-2xl"
+            />
+            <h3 className="barlow text-xl font-semibold text-[#040a17] my-3">
+              Event Specialist
+            </h3>
+            <p className="barlow text-sm font-medium text-[#a9a9a9]">CEO</p>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default OurTeam;
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import Hero from "../Components/Hero/Hero";
+import OurTeam from "../Components/OurTeam";
 import Service from "../Components/Service";
 
 const HomePage = () => {
@@ -6,6 +7,7 @@ const HomePage = () => {
         <div>
             <Hero></Hero>
             <Service></Service>
+            <OurTeam></OurTeam>
         </div>
     );
 };
